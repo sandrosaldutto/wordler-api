6050605c2341c1f989b6b15699579d55aeffc4da
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-05-25 21:16:55 -0400

    created env file

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const corsOptions = { origin: 'https://wordler-client.herokuapp.com/', optionSuccessStatus: 200 }
+const PORT = process.env.PORT || 8080;
 
-app.use(cors(corsOptions));
 app.use(express.json())
+app.use(cors());
 
 
 
 const wordRoutes = require('./routes/words');
 app.use('/words', wordRoutes);
 
-app.listen(8080, () => {
-    console.log("server is running on port 8080")
-})
+app.listen(PORT, () => {
+    console.log("server is running on port " + PORT);
+  });
+  
